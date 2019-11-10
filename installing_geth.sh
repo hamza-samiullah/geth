959b205aa032b222e49ab5859b1718308ffe658b
Hamza <hamzasamiullah@gmail.com>
2019-11-10 13:00:48 +0500

    yes
    file

---
+++ installing_geth.sh
@@ -0,0 +1,8 @@
+sudo apt-get install software-properties-common
+sudo add-apt-repository -y ppa:ethereum/ethereum
+sudo apt-get update
+sudo add-apt-repository ppa:longsleep/golang-backports
+sudo apt-get update
+sudo apt-get install golang-go
+sudo apt-get install ethereum
+geth account new
